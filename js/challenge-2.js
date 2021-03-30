a30df348b3b920d8169f9436e38c9345501e9582
ahmadaldabouqii <ahmadaldabouqi97@gmail.com>
2021-03-30 13:28:37 +0300

    Cooding Challenge

---
+++ js/challenge-2.js
@@ -0,0 +1,10 @@
+'use strict';
+
+let num_1 = prompt('enter num1');
+let num_2 = prompt('enter num2');
+let num_3 = prompt('enter num3');
+let num_4 = prompt('enter num4');
+let num_5 = prompt('enter num5');
+
+alert(num_1);
+
